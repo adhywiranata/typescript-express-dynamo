225c34cb725ffdd5ad2e274488e07fe70b66afae
Adhy <adhywiranatap@gmail.com>
2018-06-25 22:23:01 +0700

    update environment and model

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,20 +2,19 @@ const AWS = require('aws-sdk');
 
 require('dotenv').config();
 
-const REGION = 'ap-southeast-1';
 const ENDPOINT = 'https://dynamodb.ap-southeast-1.amazonaws.com';
 
 if (process.env.NODE_ENV === 'production') {
   AWS.config.update({
-    region: REGION,
-    endpoint: ENDPOINT,
+    region: process.env.REGION || 'ap-southeast-1',
+    endpoint: ENDPOINT || '',
   });
 } else {
   AWS.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
-    region: REGION,
-    endpoint: ENDPOINT,
+    region: process.env.REGION || 'ap-southeast-1',
+    endpoint: ENDPOINT || '',
   });
 }
 
